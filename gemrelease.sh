fba24580e45a68fe6ad96f58cf0238388c94ffb9
peter.abbott <pabbottnz@gmail.com>
2015-03-28 21:53:43 +1300

    setup to test autoversioning

---
+++ gemrelease.sh
@@ -0,0 +1,47 @@
+#!/bin/bash 
+
+VERSION_FILE=./VERSION
+
+[[ $(git status -s | grep -v ?? | wc -l ) -gt 0 ]] && { echo "uncommited changes, can't continue"; exit 1; } 
+RELEASE_TYPE=patch
+[ ! -z "$1" ] && RELEASE_TYPE=$1
+
+GEMSPEC_FILE=$(ls -1 *.gemspec | head -1)
+
+[ ! -f $GEMSPEC_FILE ] && { echo "no gemspec file"; exit 1; }
+[ ! -d ./vendor/bundle/ruby ] && bundle install --deployment 
+
+rm -f $VERSION_FILE && echo "Current Version is: $(bundle exec scmversion current )"
+
+[ ! -f $VERSION_FILE ] &&  { echo "no version file"; exit 1; }
+
+bundle exec scmversion bump $RELEASE_TYPE 
+[[ $? -ne 0 ]] && { echo "Version Update Failed"; exit 1; }
+
+RELEASE_VERSION=$(cat $VERSION_FILE)
+GEM_FILE=$(echo $GEMSPEC_FILE | sed 's/\.gemspec//')-${RELEASE_VERSION}.gem
+
+trap  "rm -f $GEM_FILE VERSION"  0 1 
+
+echo "Deploying Release version: $RELEASE_VERSION"
+
+[[ $? -ne 0 ]] && { echo "unable to update version file"; exit 1; } 
+[ -z "$(bundle exec gem list kitchen-docker_ssh -q | grep "($RELEASE_VERSION)" )" ] && { echo "Version not updated"; exit 1; }
+
+bundle exec gem build $GEMSPEC_FILE 
+[[ $? -ne 0 ]] && { echo "Gem Build failed"; exit 1; }
+
+if [ ! -f ~/.gem/credentials ]; then
+  cat <<-EOF > ~/.gem/credentials
+---
+:rubygems_api_key: $RUBYGEMS_API_KEY
+EOF
+  chmod 0600 ~/.gem/credentials  
+fi
+
+bundle exec gem push $GEM_FILE --key rubygems 
+[[ $? -ne 0 ]] && { echo "Gem Push failed"; exit 1; }
+
+echo "Done"
+
+exit 0
